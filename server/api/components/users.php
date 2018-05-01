7cbe4b73f7bfc2329d9ef426fe362961b83d7432
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-05-01 18:01:31 +0800

    Frame of API of /users

--- server/api/components/users.php
+++ server/api/components/users.php
@@ -1,12 +1,37 @@
 <?php
 
-switch ($requestObj["method"]) {
-    case "POST":
+if (count($requestObj["path"]) == 1)
+    switch ($requestObj["method"]) {
+        // POST /users
+        case "POST":
 
-        break;
-    default:
-        do_error(
-            405,
-            ERROR_HTTP_405);
-        break;
+            break;
+        // GET /users
+        case "GET":
+
+            break;
+        default:
+            do_error(
+                405,
+                ERROR_HTTP_405);
+            break;
+    }
+else {
+//TODO: Subpath parsing
+    $user_id = $requestObj["path"][1];
+    switch ($requestObj["method"]) {
+        // PUT /users/{id}
+        case "PUT":
+
+            break;
+        // GET /users/{id}
+        case "GET":
+
+            break;
+        default:
+            do_error(
+                405,
+                ERROR_HTTP_405);
+            break;
+    }
 }
