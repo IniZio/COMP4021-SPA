23bcfebba9f2e94bbb191d59b9e000616fbf3dd7
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-05-01 16:28:30 +0800

    Init SQL modified for Users

--- server/init.sql
+++ server/init.sql
@@ -1,43 +1,52 @@
 create table Users
 (
- id INTEGER not null,
- name TEXT not null,
- hash_salt TEXT not null,
- hashed_password TEXT not null,
- picture_id INTEGER
- );
+	id INTEGER not null,
+	username TEXT not null,
+	hash_salt TEXT not null,
+	hashed_password TEXT not null,
+	picture_file_id INTEGER,
+	first_name TEXT,
+	last_name TEXT,
+	email TEXT,
+	major TEXT,
+	year INTEGER,
+	status TEXT
+)
+;
 
-create unique index Users_Auth_id_uindex on Users (id);
+create unique index Users_Auth_id_uindex
+	on Users (id)
+;
 
-create unique index Users_Auth_name_uindex on Users (name);
+create unique index Users_Auth_username_uindex
+	on Users (username)
+;
+
+create unique index Users_email_uindex
+	on Users (email)
+;
+
+create index Users_major_index
+	on Users (major)
+;
 
 
 
 create table Resources
 (
- id INTEGER not null,
- name TEXT not null,
- course_id INTEGER not null,
- description TEXT,
- type TEXT not null,
- file_id INTEGER,
- text_content TEXT
- );
-
-create unique index Resources_id_uindex on Resources (id);
-
-
-
-create table Files
-(
 	id INTEGER not null,
-	file_name TEXT not null,
-	content_type TEXT not NULL
-);
+	name TEXT not null,
+	course_id INTEGER not null,
+	description TEXT,
+	type TEXT not null,
+	file_id INTEGER,
+	text_content TEXT
+)
+;
 
-create unique index Files_file_name_uindex on Files (file_name);
-
-create unique index Files_id_uindex on Files (id);
+create unique index Resources_id_uindex
+	on Resources (id)
+;
 
 
 
@@ -46,11 +55,16 @@ create table Courses
 	id INTEGER not null,
 	name TEXT not null,
 	descrption TEXT
-);
+)
+;
 
-create unique index Courses_id_uindex on Courses (id);
+create unique index Courses_id_uindex
+	on Courses (id)
+;
 
-create unique index Courses_name_uindex on Courses (name);
+create unique index Courses_name_uindex
+	on Courses (name)
+;
 
 
 
@@ -61,6 +75,11 @@ create table Comments
 	content TEXT not null,
 	created_timestamp INTEGER not null,
 	course_id INTEGER not null
-);
+)
+;
 
-create unique index Comments_id_uindex on Comments (id);
+create unique index Comments_id_uindex
+	on Comments (id)
+;
+
+
