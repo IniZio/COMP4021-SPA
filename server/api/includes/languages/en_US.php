142b9ced40bde75955bf6f44a5f9ef97b7d6e7c5
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-05-02 09:44:44 +0800

    feat(auth): adds auth controller and models (#12)
    * Fix API path to /api/*
    
    * Fix package.json about copying /server directory not recurrsive
    
    * Fix package.json about dev run on new php script
    
    * Fix run dev php line
    
    * Sqlite3 init db sql
    
    * API route resolve
    
    * Init SQL modified for Users
    
    * More backend stuffs
    
    * Finished Auth
    
    * Frame of API of /users
    
    * Login working
    
    * Login working
    
    * Remove User salt field, as hash field already took care of that

---
+++ server/api/includes/languages/en_US.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: signalng
+ * Date: 5/1/18
+ * Time: 5:27 PM
+ */
+
+const ERROR_PASSWORD_NOT_MATCH = "Password not match.";
+const ERROR_PARAMETER_FAULT = "Parameters not correct.";
+const ERROR_HTTP_405 = "Method not allowed.";
+const ERROR_HTTP_PATH_404 = "Path does not exist.";
+const ERROR_UNEXPECTED = "Unexpected error.";
+const ERROR_USER_NOT_LOGGEDIN = "User not logged in.";
--- server/api/index.php
+++ server/api/index.php
@@ -1,9 +1,56 @@
 <?php
+
+/*
+ * How to add a new route?
+ *
+ * Add a php file under /components,
+ * Add a case under switch, with value as the path,
+ * include_once your php file in the case,
+ * in your php file, you can use global $requestObj, $responseObj and $db.
+ *
+ * If you need anything other than json response, set $responseObj as null,
+ * and then do your own output and content_type header.
+ */
+
+session_start();
+
+include_once "./includes/utilities.php";
+include_once "./includes/languages/en_US.php";
+
 header('Access-Control-Allow-Origin: *'); 
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-header('Content-Type: text/plain');
-echo("Hello API");
-?>
+
+$requestObj = array();
+$requestObj["method"] = $_SERVER['REQUEST_METHOD'];
+$requestObj["path"] = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$requestObj["POST_JSONObj"] = json_decode(file_get_contents('php://input'),true);
+$requestObj["GET_Params"] = $_GET;
+$requestObj["SESSION_Params"] = $_SESSION;
+
+$responseObj = array();
+
+$db = new SQLite3("./data.db");
+
+switch ($requestObj["path"][0]){
+    case "users":
+        include "components/users.php";
+        break;
+    case "auth":
+        include "components/auth.php";
+        break;
+    case "courses":
+        include "components/courses.php";
+        break;
+    default:
+        do_error(
+            404,
+            ERROR_HTTP_PATH_404);
+        break;
+}
+
+do_error(404, ERROR_UNEXPECTED);
+cleanup_exit();
+
