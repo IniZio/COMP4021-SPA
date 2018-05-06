147ce03e0cb7c4118401c962ff36bb496192bc3b
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-05-06 21:47:39 +0800

    feat: all API set! :D (#19)
    * User Picture Upload
    
    * Reset User Password
    
    * PUT /api/auth case break
    
    * Fix
    
    * Fix User Pic Upload
    
    * Delete User Pic
    
    * Courses API without comments or resources
    
    * FIX OPTIONS
    
    * Course Resources/Comments Skeleton
    
    * Code Reformat
    
    * Fit into 80 col width for readability.
    
    * Fix DB Path to api/data.db
    
    * Resource/Resource File API
    
    * Comments API
    
    * Fix DB Creation SQL
    
    * Master Reset Switch
    
    * DB Creation if file not exist
    
    * Fix Update user changed password
    
    * refactor(user): returns full profile on login and register

--- server/api/components/courses.php
+++ server/api/components/courses.php
@@ -2,131 +2,126 @@
 
 if (count($path) == 1)
 	switch ($method) {
-		case "GET":
-			do_check_auth();
-			$sqlRet = do_sqlite3_prepared_statement(
-				"SELECT * FROM Courses ORDER BY name ASC ",
-				[]);
-			do_response(200, $sqlRet);
-			break;
-		case "POST":
-			do_check_auth();
-			if (is_string($post_json["name"])) {
-				if (!is_string($post_json["description"])) {
-					$post_json["description"] = null;
-				}
-				do_sqlite3_prepared_statement(
-					"INSERT INTO Courses (name, descrption) VALUES (:name,:description)",
+	case "GET":
+		do_check_auth();
+		$sqlRet = do_sqlite3_prepared_statement(
+			"SELECT * FROM Courses ORDER BY name ASC ",
+			[]);
+		do_response(200, $sqlRet);
+		break;
+	case "POST":
+		do_check_auth();
+		if (is_string($post_json["name"])) {
+			if (!is_string($post_json["description"])) {
+				$post_json["description"] = null;
+			}
+			do_sqlite3_prepared_statement(
+				"
+				INSERT INTO Courses (name, descrption) 
+				VALUES (:name,:description)",
+				[
 					[
-						array(
-							"param" => ":name",
-							"value" => $post_json["name"],
-							"type" => SQLITE3_TEXT
-						),
-						array(
-							"param" => ":description",
-							"value" => $post_json["description"],
-							"type" => SQLITE3_TEXT
-						)
+						"param" => ":name",
+						"value" => $post_json["name"],
+						"type" => SQLITE3_TEXT,
 					],
-					true);
-				$sqlRet = do_sqlite3_prepared_statement(
-					"SELECT id FROM Courses WHERE name=:name",
 					[
-						array(
-							"param" => ":name",
-							"value" => $post_json["name"],
-							"type" => SQLITE3_TEXT
-						)
-					]
-				);
-				if (!isset($sqlRet[0]["id"])) {
-					error(ERROR_UNEXPECTED);
-				}
-				do_response(201, array("id" => $sqlRet[0]["id"]));
-			} else error(ERROR_PARAMETER_FAULT);
-			break;
-		default:
-			error(ERROR_HTTP_METHOD_NOT_ALLOWED);
-			break;
+						"param" => ":description",
+						"value" => $post_json["description"],
+						"type" => SQLITE3_TEXT,
+					],
+				],
+				true);
+			$sqlRet = do_sqlite3_prepared_statement(
+				"SELECT id FROM Courses WHERE name=:name",
+				[
+					[
+						"param" => ":name",
+						"value" => $post_json["name"],
+						"type" => SQLITE3_TEXT,
+					],
+				]
+			);
+			if (!isset($sqlRet[0]["id"])) {
+				error(ERROR_UNEXPECTED);
+			}
+			do_response(201, ["id" => $sqlRet[0]["id"]]);
+		}
+		else error(ERROR_PARAMETER_FAULT);
+		break;
+	default:
+		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
+		break;
 	}
 
 $course_id = $path[1];
-$sqlRet = do_sqlite3_prepared_statement(
-	"SELECT * FROM Courses WHERE id=:id",
-	[
-		array(
-			"param" => ":id",
-			"value" => $course_id,
-			"type" => SQLITE3_INTEGER
-		)
-	]);
-if (!isset($sqlRet[0])) {
-	error(ERROR_HTTP_RESOURCE_404);
-}
-$course = $sqlRet[0];
+$course = get_resource_by_id($course_id, "Courses");
 
 if (count($path) == 2)
 	switch ($method) {
-		case "GET":
-			do_check_auth();
-			do_response(200, $course);
-			break;
-		case "PUT":
-			do_check_auth();
-			if (!is_string($post_json["name"])) {
-				$post_json["name"] = $course["name"];
-			}
-			if (!is_string($post_json["description"])) {
-				$post_json["description"] = $course["description"];
-			}
-			do_sqlite3_prepared_statement(
-				"UPDATE Courses SET name=:name, descrption=:description WHERE id=:id",
+	case "GET":
+		do_check_auth();
+		do_response(200, $course);
+		break;
+	case "PUT":
+		do_check_auth();
+		if (!is_string($post_json["name"])) {
+			$post_json["name"] = $course["name"];
+		}
+		if (!is_string($post_json["description"])) {
+			$post_json["description"] = $course["description"];
+		}
+		do_sqlite3_prepared_statement(
+			"
+			UPDATE Courses 
+			SET name=:name, descrption=:description 
+			WHERE id=:id",
+			[
 				[
-					array(
-						"param" => ":name",
-						"value" => $post_json["name"],
-						"type" => SQLITE3_TEXT
-					),
-					array(
-						"param" => ":description",
-						"value" => $post_json["description"],
-						"type" => SQLITE3_TEXT
-					),
-					array(
-						"param" => ":id",
-						"value" => $course_id,
-						"type" => SQLITE3_INTEGER
-					)
-				]
-			);
-			do_response(200);
-			break;
-		case "DELETE":
-			do_check_auth();
-			do_sqlite3_prepared_statement("DELETE FROM Courses WHERE id=:id",
+					"param" => ":name",
+					"value" => $post_json["name"],
+					"type" => SQLITE3_TEXT,
+				],
 				[
-					array(
-						"param" => ":id",
-						"value" => $course_id,
-						"type" => SQLITE3_INTEGER
-					)
-				]);
-			do_response(200);
-			break;
-		default:
-			error(ERROR_HTTP_METHOD_NOT_ALLOWED);
-			break;
+					"param" => ":description",
+					"value" => $post_json["description"],
+					"type" => SQLITE3_TEXT,
+				],
+				[
+					"param" => ":id",
+					"value" => $course_id,
+					"type" => SQLITE3_INTEGER,
+				],
+			]
+		);
+		do_response(200);
+		break;
+	case "DELETE":
+		do_check_auth();
+		do_sqlite3_prepared_statement(
+			"DELETE FROM Courses WHERE id=:id",
+			[
+				[
+					"param" => ":id",
+					"value" => $course_id,
+					"type" => SQLITE3_INTEGER,
+				],
+			]);
+		do_response(200);
+		break;
+	default:
+		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
+		break;
 	}
 
 switch ($path[2]) {
-	case "resources":
-		include "resources.php";
-		break;
-	case "comments":
-		include "comments.php";
-		break;
-	default:
-		error(ERROR_HTTP_PATH_404);
-		break;
+case "resources":
+	include "resources.php";
+	break;
+case "comments":
+	include "comments.php";
+	break;
+default:
+	error(ERROR_HTTP_PATH_404);
+	break;
 }
--- server/api/components/users.php
+++ server/api/components/users.php
@@ -3,279 +3,368 @@
 if (count($path) == 1)
 	switch ($method) {
 		// POST /users
-		case "POST":
-			if (is_authed()) {
-				error(ERROR_USER_LOGGEDIN);
-			}
-			if (is_string($post_json["username"]) &&
-				is_string($post_json["password"])) {
-				do_sqlite3_prepared_statement("
-								INSERT INTO Users 
-									(username, hashed_password) 
-								VALUES 
-									(:username, :hashed_password);",
+	case "POST":
+		if (is_authed()) {
+			error(ERROR_USER_LOGGEDIN);
+		}
+		if (is_string($post_json["username"]) &&
+			is_string($post_json["password"])) {
+			$sqlRet = do_sqlite3_prepared_statement(
+				"
+				INSERT INTO Users
+					(username, hashed_password)
+				VALUES
+					(:username, :hashed_password);",
+				[
 					[
-						array(
-							"param" => ":username",
-							"value" => $post_json["username"],
-							"type" => SQLITE3_TEXT),
-						array(
-							"param" => ":hashed_password",
-							"value" => password_hash($post_json["password"], PASSWORD_DEFAULT),
-							"type" => SQLITE3_TEXT
-						)
+						"param" => ":username",
+						"value" => $post_json["username"],
+						"type" => SQLITE3_TEXT],
+					[
+						"param" => ":hashed_password",
+						"value" => password_hash(
+							$post_json["password"], PASSWORD_DEFAULT),
+						"type" => SQLITE3_TEXT,
 					],
-					true);
+				],
+				true,
+				true);
+			if ($sqlRet[0] != 0) {
+				switch ($sqlRet[0]) {
+				case 19:
+					error(ERROR_USER_ALREADY_EXIST);
+					break;
+				default:
+					error(ERROR_UNEXPECTED);
+					break;
+				}
+			}
 
-				$userEntries = do_sqlite3_prepared_statement("
-								SELECT id 
-                    			FROM Users 
-                    			WHERE username=:username;",
+			$userEntries = do_sqlite3_prepared_statement(
+				"
+				SELECT *
+				FROM Users
+				WHERE username=:username;",
+				[
 					[
-						array(
-							"param" => ":username",
-							"value" => $post_json["username"],
-							"type" => SQLITE3_TEXT)
-					]);
-				$responseObj = array();
-				$responseObj["id"] = $userEntries[0]["id"];
+						"param" => ":username",
+						"value" => $post_json["username"],
+						"type" => SQLITE3_TEXT],
+        ]);
+      $userEntry = $userEntries[0];
+      unset($userEntry["hashed_password"]);
 
-				do_response(201, $responseObj);
-			} else
-				error(ERROR_PARAMETER_FAULT);
-			break;
+      $_SESSION["user"] = $userEntry;
+      $responseObj = [];
+			$responseObj["user"] = $userEntry;
+			$responseObj["id"] = $userEntries[0]["id"];
+
+			do_response(201, $responseObj);
+		}
+		else
+			error(ERROR_PARAMETER_FAULT);
+		break;
 		// GET /users
-		case "GET":
-			do_check_auth();
-			do_response(
-				200,
-				do_sanitize_user_info($_SESSION["user"]));
-			break;
-		// PUT /users
-		case "PUT":
-			break;
-		default:
-			error(ERROR_HTTP_METHOD_NOT_ALLOWED);
-			break;
+	case "GET":
+		do_check_auth();
+		do_response(
+			200,
+			do_sanitize_user_info($_SESSION["user"]));
+		break;
+	default:
+		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
+		break;
 	}
 
 $user_id = (int)$path[1];
 if (count($path) == 2)
 	switch ($method) {
 		// PUT /users/{id}
-		case "PUT":
-			do_check_auth();
-			if ($user_id == $_SESSION["user"]["id"]) {
-				do_sqlite3_prepared_statement("
-								UPDATE Users 
-								SET 
-									first_name=:first_name,
-									last_name=:last_name,
-									email=:email, 
-									hashed_password=:hashed_password,
-									major=:major,
-									year=:year, 
-									status=:status 
-								WHERE id=:id;",
+	case "PUT":
+		do_check_auth();
+		if ($user_id == $_SESSION["user"]["id"]) {
+			$post_json["first_name"] =
+				is_string($post_json["first_name"]) ?
+					$post_json["first_name"] :
+					$_SESSION["user"]["first_name"];
+
+			$post_json["last_name"] =
+				is_string($post_json["last_name"]) ?
+					$post_json["last_name"] :
+					$_SESSION["user"]["last_name"];
+
+			$post_json["email"] =
+				is_string($post_json["email"]) ?
+					$post_json["email"] :
+					$_SESSION["user"]["email"];
+
+			$post_json["major"] =
+				is_string($post_json["major"]) ?
+					$post_json["major"] :
+					$_SESSION["user"]["major"];
+
+			$post_json["year"] =
+				is_integer($post_json["year"]) ?
+					$post_json["year"] :
+					$_SESSION["user"]["year"];
+
+			$post_json["status"] =
+				is_string($post_json["status"]) ?
+					$post_json["status"] :
+					$_SESSION["user"]["status"];
+
+
+			do_sqlite3_prepared_statement(
+				"
+				UPDATE Users
+				SET
+					first_name=:first_name,
+					last_name=:last_name,
+					email=:email,
+					major=:major,
+					year=:year,
+					status=:status
+				WHERE id=:id;",
+				[
 					[
-						array(
-							"param" => ":first_name",
-							"value" => $post_json["first_name"],
-							"type" => SQLITE3_TEXT),
-						array(
-							"param" => ":last_name",
-							"value" => $post_json["last_name"],
-							"type" => SQLITE3_TEXT),
-						array(
-							"param" => ":email",
-							"value" => $post_json["email"],
-							"type" => SQLITE3_TEXT),
-						array(
-							"param" => ":hashed_password",
-							"value" => password_hash($post_json["password"], PASSWORD_DEFAULT),
-							"type" => SQLITE3_TEXT
-						),
-						array(
-							"param" => ":major",
-							"value" => $post_json["major"],
-							"type" => SQLITE3_TEXT),
-						array(
-							"param" => ":year",
-							"value" => $post_json["year"],
-							"type" => SQLITE3_INTEGER),
-						array(
-							"param" => ":status",
-							"value" => $post_json["status"],
-							"type" => SQLITE3_TEXT),
-						array(
-							"param" => ":id",
-							"value" => $user_id,
-							"type" => SQLITE3_INTEGER),
+						"param" => ":first_name",
+						"value" => $post_json["first_name"],
+						"type" => SQLITE3_TEXT,
 					],
-					true
-				);
-				$_SESSION["user"] = do_sqlite3_prepared_statement(
-					"SELECT * FROM Users WHERE id=:id",
-					[array(
+					[
+						"param" => ":last_name",
+						"value" => $post_json["last_name"],
+						"type" => SQLITE3_TEXT,
+					],
+					[
+						"param" => ":email",
+						"value" => $post_json["email"],
+						"type" => SQLITE3_TEXT,
+					],
+					[
+						"param" => ":major",
+						"value" => $post_json["major"],
+						"type" => SQLITE3_TEXT,
+					],
+					[
+						"param" => ":year",
+						"value" => $post_json["year"],
+						"type" => SQLITE3_INTEGER,
+					],
+					[
+						"param" => ":status",
+						"value" => $post_json["status"],
+						"type" => SQLITE3_TEXT,
+					],
+					[
 						"param" => ":id",
 						"value" => $user_id,
-						"type" => SQLITE3_INTEGER)]
-				)[0];
-				do_response(200);
-			} else
-				error(ERROR_USER_NOT_MATCH);
-			break;
+						"type" => SQLITE3_INTEGER,
+					],
+				],
+				true
+			);
+			if (is_string($post_json["password"])) {
+				do_sqlite3_prepared_statement(
+					"
+					UPDATE Users
+					SET hashed_password=:hashed_password
+					WHERE id=:id",
+					[
+						[
+							"param" => ":hashed_password",
+							"value" => password_hash(
+								$post_json["password"], PASSWORD_DEFAULT),
+							"type" => SQLITE3_TEXT,
+						],
+						[
+							"param" => ":id",
+							"value" => $user_id,
+							"type" => SQLITE3_INTEGER,
+						],
+					],
+					true);
+			}
+			$userEntry = do_sqlite3_prepared_statement(
+				"SELECT * FROM Users WHERE id=:id",
+				[[
+					"param" => ":id",
+					"value" => $user_id,
+					"type" => SQLITE3_INTEGER]]
+      )[0];
+      unset($userEntry["hashed_password"]);
+
+      $_SESSION["user"] = $userEntry;
+      $responseObj = [];
+			$responseObj["user"] = $userEntry;
+			do_response(200);
+		}
+		else
+			error(ERROR_USER_NOT_MATCH);
+		break;
 		// GET /users/{id}
-		case "GET":
-			do_check_auth();
-			if ($user_id == $_SESSION["user"]["id"])
-				do_response(
-					200,
-					do_sanitize_user_info($_SESSION["user"]));
-			else
-				error(ERROR_USER_NOT_MATCH);
-			break;
-		default:
-			error(ERROR_HTTP_METHOD_NOT_ALLOWED);
-			break;
+	case "GET":
+		do_check_auth();
+		if ($user_id == $_SESSION["user"]["id"])
+			do_response(
+				200,
+				do_sanitize_user_info($_SESSION["user"]));
+		else
+			error(ERROR_USER_NOT_MATCH);
+		break;
+	default:
+		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
+		break;
 	}
 
 
 if (count($path) == 3 &&
 	$path[2] === "picture") {
 	switch ($method) {
-		case "GET":
-			do_check_auth();
-			if ($_SESSION["user"]["picture_file_id"] === null) {
-				error(ERROR_HTTP_FILE_404);
-			}
-			$fileEntries = do_sqlite3_prepared_statement(
-				"SELECT file_name, content_type FROM Files WHERE id=:id",
+	case "GET":
+		do_check_auth();
+		if ($_SESSION["user"]["picture_file_id"] === null) {
+			error(ERROR_HTTP_FILE_404);
+		}
+		$fileEntries = do_sqlite3_prepared_statement(
+			"SELECT file_name, content_type FROM Files WHERE id=:id",
+			[
 				[
-					array(
-						"param" => ":id",
-						"value" => $_SESSION["user"]["picture_file_id"],
-						"type" => SQLITE3_TEXT
-					)
-				]
-			);
-			if (count($fileEntries) < 1) {
-				error(ERROR_HTTP_FILE_404);
-			}
-			if (file_exists(FILEDIR . $fileEntries[0]["file_name"])) {
-				header('Content-Type: ' . $fileEntries[0]["content_type"]);
-				readfile(FILEDIR . $fileEntries[0]["file_name"]);
-			} else
-				error(ERROR_HTTP_FILE_404);
-			exit();
-			break;
-		case "POST":
-			do_check_auth();
-			if (!isset($_FILES["file"])) {
-				error(ERROR_PARAMETER_FAULT, array("file" => "Post a picture with a form, with file input field id 'file'."));
-			}
-			if ($_FILES["file"]["error"] !== UPLOAD_ERR_OK) {
-				error(ERROR_UPLOAD_FILE_FAILED);
-			}
-			if (!in_array($_FILES["file"]["type"], accepted_image_mime)) {
-				error(ERROR_PARAMETER_FAULT, array("file" => "Image format not accepted."));
-			}
-			$file_name = "";
-			do {
-				$file_name = date("y-m-d-H:i:s") . $_FILES["file"]["name"];
-				$sqlRet = do_sqlite3_prepared_statement(
-					"INSERT INTO Files (file_name, content_type) VALUES (:file_name, :content_type)",
-					[
-						array(
-							"param" => ":file_name",
-							"value" => $file_name,
-							"type" => SQLITE3_TEXT
-						),
-						array(
-							"param" => ":content_type",
-							"value" => $_FILES["file"]["type"],
-							"type" => SQLITE3_TEXT
-						)
-					],
-					true, true);
-			} while ($sqlRet !== null);
-			if (!move_uploaded_file($_FILES["file"]["tmp_name"], FILEDIR . $file_name)) {
-				error(ERROR_UPLOAD_FILE_FAILED);
-			}
+					"param" => ":id",
+					"value" => $_SESSION["user"]["picture_file_id"],
+					"type" => SQLITE3_TEXT,
+				],
+			]
+		);
+		if (count($fileEntries) < 1) {
+			error(ERROR_HTTP_FILE_404);
+		}
+		if (file_exists(FILEDIR . $fileEntries[0]["file_name"])) {
+			header('Content-Type: ' . $fileEntries[0]["content_type"]);
+			readfile(FILEDIR . $fileEntries[0]["file_name"]);
+		}
+		else
+			error(ERROR_HTTP_FILE_404);
+		exit();
+		break;
+	case "POST":
+		do_check_auth();
+		if (!isset($_FILES["file"])) {
+			error(
+				ERROR_PARAMETER_FAULT,
+				["file" => "Post a picture with a form, with file input " .
+					"field id 'file'."]);
+		}
+		if ($_FILES["file"]["error"] !== UPLOAD_ERR_OK) {
+			error(ERROR_UPLOAD_FILE_FAILED);
+		}
+		if (!in_array($_FILES["file"]["type"], accepted_image_mime)) {
+			error(
+				ERROR_PARAMETER_FAULT,
+				["file" => "Image format not accepted."]);
+		}
+		$file_name = "";
+		do {
+			$file_name = date("y-m-d-H:i:s") . $_FILES["file"]["name"];
 			$sqlRet = do_sqlite3_prepared_statement(
-				"SELECT id FROM Files WHERE file_name=:file_name",
+				"
+				INSERT INTO Files (file_name, content_type)
+				VALUES (:file_name, :content_type)",
 				[
-					array(
+					[
 						"param" => ":file_name",
 						"value" => $file_name,
-						"type" => SQLITE3_TEXT
-					)
-				]
-			);
-			if (!isset($sqlRet[0]["id"])) {
-				error(ERROR_UNEXPECTED);
-			}
-			do_sqlite3_prepared_statement(
-				"UPDATE Users SET picture_file_id=:pic_id WHERE id=:user_id",
-				[
-					array(
-						"param" => ":pic_id",
-						"value" => $sqlRet[0]["id"],
-						"type" => SQLITE3_INTEGER
-					),
-					array(
-						"param" => ":user_id",
-						"value" => $_SESSION["user"]["id"],
-						"type" => SQLITE3_INTEGER
-					)
-				]
-			);
-			$_SESSION["user"]["picture_file_id"] = $sqlRet[0]["id"];
-			do_response(201);
-			break;
-		case "DELETE":
-			do_check_auth();
-			$sqlRet = do_sqlite3_prepared_statement(
-				"SELECT id,file_name FROM Files WHERE id=:pic_id",
-				[
-					array(
-						"param" => ":pic_id",
-						"value" => $_SESSION["user"]["picture_file_id"],
-						"type" => SQLITE3_INTEGER
-					)
-				]
-			);
-			if (!isset($sqlRet[0])){
-				error(ERROR_UNEXPECTED);
-			}
-			$_SESSION["user"]["picture_file_id"] = null;
-			do_sqlite3_prepared_statement(
-				"UPDATE Users SET picture_file_id=null WHERE id=:id",
-				[
-					array(
-						"param" => ":id",
-						"value" => $_SESSION["user"]["id"],
-						"type" => SQLITE3_INTEGER
-					)
+						"type" => SQLITE3_TEXT,
+					],
+					[
+						"param" => ":content_type",
+						"value" => $_FILES["file"]["type"],
+						"type" => SQLITE3_TEXT,
+					],
 				],
-				true
-			);
-			do_sqlite3_prepared_statement(
-				"DELETE FROM Files WHERE id=:pic_id",
+				true, true);
+		} while ($sqlRet !== null);
+		if (!move_uploaded_file(
+			$_FILES["file"]["tmp_name"], FILEDIR . $file_name)) {
+			error(ERROR_UPLOAD_FILE_FAILED);
+		}
+		$sqlRet = do_sqlite3_prepared_statement(
+			"SELECT id FROM Files WHERE file_name=:file_name",
+			[
 				[
-					array(
-						"param" => ":pic_id",
-						"value" => $sqlRet[0]["id"],
-						"type" => SQLITE3_INTEGER
-					)
+					"param" => ":file_name",
+					"value" => $file_name,
+					"type" => SQLITE3_TEXT,
 				],
-				true
-			);
-			unlink(FILEDIR.$sqlRet[0]["file_name"]);
-			do_response(200);
-			break;
-		default:
-			error(ERROR_HTTP_METHOD_NOT_ALLOWED);
-			break;
+			]
+		);
+		if (!isset($sqlRet[0]["id"])) {
+			error(ERROR_UNEXPECTED);
+		}
+		do_sqlite3_prepared_statement(
+			"
+			UPDATE Users
+			SET picture_file_id=:pic_id
+			WHERE id=:user_id",
+			[
+				[
+					"param" => ":pic_id",
+					"value" => $sqlRet[0]["id"],
+					"type" => SQLITE3_INTEGER,
+				],
+				[
+					"param" => ":user_id",
+					"value" => $_SESSION["user"]["id"],
+					"type" => SQLITE3_INTEGER,
+				],
+			]
+		);
+		$_SESSION["user"]["picture_file_id"] = $sqlRet[0]["id"];
+		do_response(201);
+		break;
+	case "DELETE":
+		do_check_auth();
+		$sqlRet = do_sqlite3_prepared_statement(
+			"SELECT id,file_name FROM Files WHERE id=:pic_id",
+			[
+				[
+					"param" => ":pic_id",
+					"value" => $_SESSION["user"]["picture_file_id"],
+					"type" => SQLITE3_INTEGER,
+				],
+			]
+		);
+		if (!isset($sqlRet[0])) {
+			error(ERROR_UNEXPECTED);
+		}
+		$_SESSION["user"]["picture_file_id"] = null;
+		do_sqlite3_prepared_statement(
+			"UPDATE Users SET picture_file_id=null WHERE id=:id",
+			[
+				[
+					"param" => ":id",
+					"value" => $_SESSION["user"]["id"],
+					"type" => SQLITE3_INTEGER,
+				],
+			],
+			true
+		);
+		do_sqlite3_prepared_statement(
+			"DELETE FROM Files WHERE id=:pic_id",
+			[
+				[
+					"param" => ":pic_id",
+					"value" => $sqlRet[0]["id"],
+					"type" => SQLITE3_INTEGER,
+				],
+			],
+			true
+		);
+		unlink(FILEDIR . $sqlRet[0]["file_name"]);
+		do_response(200);
+		break;
+	default:
+		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
+		break;
 	}
 }
