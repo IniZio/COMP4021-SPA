7a18bda14b63546a927f3e47621467f8ba1ad59b
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-05-06 12:15:43 +0800

    Courses API without comments or resources

--- server/api/includes/languages/en_US.php
+++ server/api/includes/languages/en_US.php
@@ -12,6 +12,7 @@ const ERROR_HTTP_METHOD_NOT_ALLOWED = [405, "Method not allowed."];
 const ERROR_HTTP_PATH_404 = [404, "Path does not exist."];
 const ERROR_HTTP_FILE_404 = [404, "File does not exist."];
 const ERROR_HTTP_USER_404 = [404, "User does not exist."];
+const ERROR_HTTP_RESOURCE_404 = [404, "Resource requested does not exist."];
 const ERROR_UNEXPECTED = [404, "Unexpected error."];
 const ERROR_USER_NOT_LOGGEDIN = [401, "User not logged in."];
 const ERROR_USER_NOT_MATCH = [401, "User not match."];
