a7e48415206a268ac6bf09b6407f6077cd392497
Ben NG Hok Chun <hcngac@connect.ust.hk>
2018-05-09 07:01:54 +0800

    fix: patches for Course Search API (#25)
    * User Register First/Last Name
    
    * Fix undefined password field php notice
    
    * Fix cannot get id issue
    
    * Comments SQL Malform Fix
    
    * Course search and field changes
    
    * Fix double add comment
    
    * Fix double add comment
    
    * Fix SQL init script problem

--- server/api/components/comments.php
+++ server/api/components/comments.php
@@ -6,13 +6,13 @@ if (count($path) == 3)
 		do_check_auth();
 		$resources = do_sqlite3_prepared_statement(
 			"
-			SELECT
-				id,
-				author_user_id,
-				content,
-				created_timestamp,
+			SELECT 
+				id, 
+				author_user_id, 
+				content, 
+				created_timestamp, 
 				course_id
-			FROM Comments
+			FROM Comments 
 			WHERE course_id=:course_id",
 			[
 				[
@@ -26,9 +26,7 @@ if (count($path) == 3)
 		break;
 	case "POST":
 		do_check_auth();
-		if (
-			!is_integer($post_json["course_id"]) &&
-			!is_string($post_json["content"])) {
+		if (!is_string($post_json["content"])) {
 			error(ERROR_PARAMETER_FAULT);
 		}
 		$created_timestamp = time();
@@ -63,10 +61,11 @@ if (count($path) == 3)
 				],
 				[
 					"param" => ":course_id",
-					"value" => $post_json["course_id"],
+					"value" => $course_id,
 					"type" => SQLITE3_INTEGER,
 				],
-			]);
+			],
+			true);
 		do_response(201);
 		break;
 	default:
@@ -93,7 +92,8 @@ if (count($path) == 4)
 					"value" => $comment_id,
 					"type" => SQLITE3_INTEGER,
 				],
-			]
+			],
+			true
 		);
 		do_response(200);
 		break;
