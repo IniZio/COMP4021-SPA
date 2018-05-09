69d30de718c812614f80a2379da9554bf7c2606f
IniZio <newman.kcchow@gmail.com>
2018-05-09 09:17:41 +0000

    Automatically update from travis-ci

--- api/components/comments.php
+++ api/components/comments.php
@@ -146,3 +146,156 @@ if (count($path) == 4)
 		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
 		break;
 	}
+
+
+if (count($path) == 5 &&
+	$path[4] === "picture") {
+	switch ($method) {
+	case "GET":
+		do_check_auth();
+		if ($comment["picture_file_id"] === null) {
+			error(ERROR_HTTP_FILE_404);
+		}
+		$fileEntries = do_sqlite3_prepared_statement(
+			"SELECT file_name, content_type FROM Files WHERE id=:id",
+			[
+				[
+					"param" => ":id",
+					"value" => $comment["picture_file_id"],
+					"type" => SQLITE3_TEXT,
+				],
+			]
+		);
+		if (count($fileEntries) < 1) {
+			error(ERROR_HTTP_FILE_404);
+		}
+		if (file_exists(FILEDIR . $fileEntries[0]["file_name"])) {
+			header('Content-Type: ' . $fileEntries[0]["content_type"]);
+			readfile(FILEDIR . $fileEntries[0]["file_name"]);
+		}
+		else
+			error(ERROR_HTTP_FILE_404);
+		exit();
+		break;
+	case "POST":
+		do_check_auth();
+		if (!isset($_FILES["file"])) {
+			error(
+				ERROR_PARAMETER_FAULT,
+				["file" => "Post a picture with a form, with file input " .
+					"field id 'file'."]);
+		}
+		if ($_FILES["file"]["error"] !== UPLOAD_ERR_OK) {
+			error(ERROR_UPLOAD_FILE_FAILED);
+		}
+		if (!in_array($_FILES["file"]["type"], accepted_image_mime)) {
+			error(
+				ERROR_PARAMETER_FAULT,
+				["file" => "Image format not accepted."]);
+		}
+		$file_name = "";
+		do {
+			$file_name = date("ymdHis") . $_FILES["file"]["name"];
+			$sqlRet = do_sqlite3_prepared_statement(
+				"
+				INSERT INTO Files (file_name, content_type)
+				VALUES (:file_name, :content_type)",
+				[
+					[
+						"param" => ":file_name",
+						"value" => $file_name,
+						"type" => SQLITE3_TEXT,
+					],
+					[
+						"param" => ":content_type",
+						"value" => $_FILES["file"]["type"],
+						"type" => SQLITE3_TEXT,
+					],
+				],
+				true, true);
+		} while ($sqlRet !== null);
+		if (!move_uploaded_file(
+			$_FILES["file"]["tmp_name"], FILEDIR . $file_name)) {
+			error(ERROR_UPLOAD_FILE_FAILED);
+		}
+		$sqlRet = do_sqlite3_prepared_statement(
+			"SELECT id FROM Files WHERE file_name=:file_name",
+			[
+				[
+					"param" => ":file_name",
+					"value" => $file_name,
+					"type" => SQLITE3_TEXT,
+				],
+			]
+		);
+		if (!isset($sqlRet[0]["id"])) {
+			error(ERROR_UNEXPECTED);
+		}
+		do_sqlite3_prepared_statement(
+			"
+			UPDATE Comments
+			SET picture_file_id=:pic_id
+			WHERE id=:comment_id",
+			[
+				[
+					"param" => ":pic_id",
+					"value" => $sqlRet[0]["id"],
+					"type" => SQLITE3_INTEGER,
+				],
+				[
+					"param" => ":comment_id",
+					"value" => $comment_id,
+					"type" => SQLITE3_INTEGER,
+				],
+			],
+			true
+		);
+		$_SESSION["user"]["picture_file_id"] = $sqlRet[0]["id"];
+		do_response(201);
+		break;
+	case "DELETE":
+		do_check_auth();
+		$sqlRet = do_sqlite3_prepared_statement(
+			"SELECT id,file_name FROM Files WHERE id=:pic_id",
+			[
+				[
+					"param" => ":pic_id",
+					"value" => $_SESSION["user"]["picture_file_id"],
+					"type" => SQLITE3_INTEGER,
+				],
+			]
+		);
+		if (!isset($sqlRet[0])) {
+			error(ERROR_UNEXPECTED);
+		}
+		$_SESSION["user"]["picture_file_id"] = null;
+		do_sqlite3_prepared_statement(
+			"UPDATE Comments SET picture_file_id=null WHERE id=:id",
+			[
+				[
+					"param" => ":id",
+					"value" => $comment_id,
+					"type" => SQLITE3_INTEGER,
+				],
+			],
+			true
+		);
+		do_sqlite3_prepared_statement(
+			"DELETE FROM Files WHERE id=:pic_id",
+			[
+				[
+					"param" => ":pic_id",
+					"value" => $sqlRet[0]["id"],
+					"type" => SQLITE3_INTEGER,
+				],
+			],
+			true
+		);
+		unlink(FILEDIR . $sqlRet[0]["file_name"]);
+		do_response(200);
+		break;
+	default:
+		error(ERROR_HTTP_METHOD_NOT_ALLOWED);
+		break;
+	}
+}
